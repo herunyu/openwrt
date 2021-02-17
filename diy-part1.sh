cbdaf4621a398ea841290a707b1601ab54b07022
Runyu He <herunyu@outlook.com>
2021-02-17 19:54:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 #https://github.com/xiaorouji/openwrt-passwall
