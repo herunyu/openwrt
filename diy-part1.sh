85d6ac27d5394c567af8acc3165baa7e8281e2ef
Runyu He <herunyu@outlook.com>
2021-03-03 13:24:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,8 @@
 
 # git clone https://github.com/jerrykuku/node-request.git ./package/lean/node-request #git node-request 依赖
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
-rm -rf ./package/lean/luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon  
+# rm -rf ./package/lean/luci-theme-argon  
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon  
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
