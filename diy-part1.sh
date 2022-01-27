e9d3bd378011cefa2d400ef8bed91c7f181534a5
HYT <herunyu@outlook.com>
2022-01-27 17:26:03 +0800

    Create diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,20 +14,21 @@
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon  
 
 # Uncomment a feed source
+sed -i "/helloworld/d" "feeds.conf.default"
 
 # Add theme
 sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
 ' feeds.conf.default
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-rm -rf ./package/lean/luci-app-jd-dailybonus 
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
+
+# rm -rf ./package/lean/luci-app-jd-dailybonus 
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
 #https://github.com/xiaorouji/openwrt-passwall
 
